f175d5144303e118d8463cf506fda1e35147371d
Diego Gómez <diegevelascomuy@gmail.com>
2020-06-16 01:48:11 -0600

    Navigation Bar finished

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,11 +1,45 @@
 import React, {Component} from 'react';
 import './styles/NavigationBar.css';
+import {
+    BrowserRouter as Router,
+    Link
+  } from "react-router-dom";
 
 export default class NavigationBar extends Component{
     render(){
         return (
             <nav className="nav-bar">
-                hola
+                <h1 className="logo">CarSales PRO</h1>
+                <Router>
+                    <ul className="menu">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/buy">Buy</Link>
+                        </li>
+                        <li>
+                            <Link to="/buyers">Buyers</Link>
+                        </li>
+                        <li>
+                            <Link to="/vehicles">Vehicles</Link>
+                        </li>
+                        <li className="down-menu">
+                            Delete
+                            <ul>
+                                <li>
+                                    <Link to="/delete/buy">Buys</Link>
+                                </li>
+                                <li>
+                                    <Link to="/delete/buyers">Buyers</Link>
+                                </li>
+                                <li>
+                                    <Link to="/delete/vehicles">Vehicles</Link>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </Router>
             </nav>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'sanitize.css';
+import './index.css';
 import Home from './pages/Home';
 
 ReactDOM.render(
