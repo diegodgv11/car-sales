500e8bc5aebe1c2f135c0afe79f87122762ef715
Diego Gómez <diegevelascomuy@gmail.com>
2020-06-15 23:56:38 -0600

    All the pages have been created

---
+++ src/pages/Buyers.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Buyers extends Component{
+    render(){
+        return (
+            <div className="buyers">
+
+            </div>
+        );
+    }
+} 
---
+++ src/pages/deletes/DeleteBuy.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class DeleteBuy extends Component{
+    render(){
+        return (
+            <div className="delete-buy">
+
+            </div>
+        );
+    }
+} 
