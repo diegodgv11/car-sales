c52642231adb675b9e134f567440c0ed14c94a84
Diego Gómez <diegevelascomuy@gmail.com>
2020-06-16 00:52:21 -0600

    added sanitize.css

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './styles/NavigationBar.css';
+
+export default class NavigationBar extends Component{
+    render(){
+        return (
+            <nav className="nav-bar">
+                hola
+            </nav>
+        );
+    }
+} 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import NavigationBar from '../components/NavigationBar'; 
 
 export default class Home extends Component{
     render(){
         return (
-            <div className="home">
-
-            </div>
+            <>
+                <NavigationBar/>
+            </>
         );
     }
 } 
